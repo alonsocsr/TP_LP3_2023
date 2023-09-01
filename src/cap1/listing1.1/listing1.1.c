1abf4a98146e0684246342272e139d6464814153
root <root@DESKTOP-LLH93L3>
2023-09-01 18:34:15 -0400

    listing 1.1 1.2 y 1.3

--- src/cap1/listing1.1/listing1.1.c
+++ src/cap1/listing1.1/listing1.1.c
@@ -1,3 +1,11 @@
-int main(){
+#include<stdio.h>
+#include "reciprocal.hpp"
+
+int main(int argc, char **argv){
+    int i;
+
+    i = atoi (argv[1]);
+    printf("the reciprocal of %d is %g\n", i, reciprocal(i));
+    return 0;
     
 }
