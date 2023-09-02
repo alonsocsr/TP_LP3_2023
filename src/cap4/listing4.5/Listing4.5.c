038aaff9ead20fb4dba5c36d44a0b3264691794d
césar <cesaralonso99@gmail.com>
2023-09-02 14:58:07 -0400

    listing cap4

--- src/cap4/listing4.5/Listing4.5.c
+++ src/cap4/listing4.5/Listing4.5.c
@@ -1,21 +1,39 @@
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/*Detached threads are often used for tasks that need to run in the background or for lightweight threads where the program doesn't need to wait for their completion. Examples include background data processing, logging, or daemon-like tasks.*/
+
 void *thread_function(void *thread_arg)
 {
-  /* Do work here... */
+  printf("Detached thread started.\n");
+  sleep(3); // Simulate some work
+  printf("Detached thread finished.\n");
+  return NULL;
 }
 int main()
 {
   pthread_attr_t attr;
   pthread_t thread;
 
+  // Initialize thread attributes with the default values
   pthread_attr_init(&attr);
+
+  // Set the thread as detached
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
   
+  // Create the detached thread
   pthread_create(&thread, &attr, &thread_function, NULL);
+  
+  // Main thread continues its work
+  printf("Main thread continues.\n");
+
+  // Clean up the thread attribute
   pthread_attr_destroy(&attr);
-  /* Do work here...
-   */
-  /* No need to join the second thread.
+
+  // Sleep to allow the detached thread to finish (optional)
+  sleep(4);
+
+  /* No need to join the second thread.*/
   return 0;
-  */
 }
