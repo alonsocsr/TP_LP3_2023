9414b1a2197e1cf51cebed82c693da9a8aad8b9e
Marcos <Marcosrflores737@gmail.com>
2023-09-17 16:01:10 -0400

    listing 2.1

--- src/cap2/listing2.1/listing2.1.c
+++ src/cap2/listing2.1/listing2.1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main (int argc, char* argv[]){
-    printf ("The name of this program is ‘%s’.\n”, argv[0]");
+    printf ("The name of this program is ‘%s’.\n", argv[0]);
     printf ("This program was invoked with %d arguments.\n", argc - 1);
     /* Were any command-line arguments specified? */
     if (argc > 1) {
@@ -10,5 +10,5 @@ int main (int argc, char* argv[]){
         for (i = 1; i < argc; ++i)
             printf (" %s\n", argv[i]);
     }
-    return 0;
+return 0;
 }
