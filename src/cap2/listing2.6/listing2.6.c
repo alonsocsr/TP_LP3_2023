aef8cc975c03fea2d7cba924e73c01ec76532e5b
Marcos <Marcosrflores737@gmail.com>
2023-09-19 14:07:03 -0400

    listing2.6

--- src/cap2/listing2.6/listing2.6.c
+++ src/cap2/listing2.6/listing2.6.c
@@ -39,16 +39,13 @@ char* read_from_file (const char* filename, size_t length){
 }
 
 int main(){
-    const char* nombre_archivo = "archivo.txt";  // Reemplaza con el nombre de tu archivo
-    size_t tamaño = 100;  // Reemplaza con la longitud deseada
+    const char* nombre_archivo = "archivo.txt";
+    size_t tamaño = 100;  
 
     char* datos = read_from_file(nombre_archivo, tamaño);
 
     if (datos != NULL) {
-        // Procesa los datos leídos, por ejemplo, imprímelos en la consola
         printf("Datos leídos del archivo: %s\n", datos);
-
-        // Libera la memoria del buffer después de usar los datos
         free(datos);
     } else {
         printf("Error al leer el archivo.\n");
